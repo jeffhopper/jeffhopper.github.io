6c83c6c4c303becdbab81294db6c3afca10f1cca
jeffhopper <jeffhopperr@gmail.com>
2016-09-26 18:10:06 -0400

    VERSION 2.1
    mobile ready

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 jQuery(document).ready(function($) {
-    $('#save a').click(function() {
-        var center = $('#blueprint');
+    $('#menu a').click(function() {
+        var center = $('#navigation');
         $('html,body').animate({ scrollTop: center.offset().top }, 400);
+    });
+
+    $('#video-scroll-play').click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 400);
+        $('#video').get(0).play();
+    });
+
+    $('video').hover(function toggleControls() {
+        if (this.hasAttribute("controls")) {
+            this.removeAttribute("controls")
+        } else {
+            this.setAttribute("controls", "controls")
+        }
     });
 
 });
