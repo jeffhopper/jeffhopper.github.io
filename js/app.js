598a1ade6da4dcbbf8efa39671914c46368406dd
jeffhopper <jeffhopperr@gmail.com>
2017-06-19 16:01:00 -0400

    Redesign 1
    Complete website redesign.
    Working draft.  Awaiting responsive overhaul

--- js/app.js
+++ js/app.js
@@ -1,27 +1,103 @@
-jQuery(document).ready(function($) {
-    $('#menu a').click(function() {
-        var center = $('#navigation');
-        $('html,body').animate({ scrollTop: center.offset().top }, 400);
+document.addEventListener("DOMContentLoaded", function() {
+
+// LOADER
+    function onReady(callback) {
+        var intervalID = window.setInterval(checkReady, 4000);
+
+        function checkReady() {
+            if (document.getElementsByTagName('body')[0] !== undefined) {
+                window.clearInterval(intervalID);
+                callback.call(this);
+            }
+        }
+    }
+
+    function show(id, value) {
+        document.getElementById(id).style.display = value ? 'block' : 'none';
+    }
+
+    onReady(function() {
+        show('wrapper', true);
+        show('loader', false);
     });
 
-    $('#video-scroll-play').click(function() {
-        $('html,body').animate({ scrollTop: 0 }, 400);
-        $('#video').get(0).play();
+
+    // CONTENT FUNCTIONS
+
+    var menuButton = document.getElementById('menu-button');
+    var contentContainer = document.getElementById('content');
+    var title = document.getElementById('title');
+    var backButton = document.getElementById('back');
+
+    // SLIDE UP CONTENT
+    menuButton.addEventListener('click', function() {
+        title.className += ' fade';
+        menuButton.className += ' fade';
+        contentContainer.className += ' show';
     });
 
-    $('video').hover(function toggleControls() {
-        if (this.hasAttribute("controls")) {
-            this.removeAttribute("controls")
+    // SLIDE DOWN CONTNET 
+    backButton.addEventListener('click', function() {
+        menuButton.className -= ' fade';
+        title.className -= ' fade';
+        contentContainer.className -= ' show';
+    });
+
+    // MUTE BUTTON
+    var video = document.getElementById('sbb-video');
+    var volumeButton = document.getElementById('volume-button');
+    volumeButton.addEventListener('click', function() {
+        if (video.muted == false) {
+            video.muted = true
         } else {
-            this.setAttribute("controls", "controls")
+            video.muted = false
         }
     });
 
-    $('a#modal-button').click(function() {
-        $('#modal').addClass('visible');
-    });
 
-    $('#close-button').click(function() {
-        $('#modal').removeClass('visible');
-    });
+    // SLIDE UP CONTENT
+    // $('#menu-button').click(function() {
+    //     $('#menu-button').fadeOut(500)
+    //     $('#content').addClass('show')
+    //         // $('h1').addClass('fade-in')
+    //     $('h1').hide().delay(600).fadeIn(300)
+    // });
+
+
+    // SLIDE DOWN CONTENT
+    // $('#back').click(function() {
+    //     $('#menu-button').delay(300).fadeIn(300)
+    //     $('h1').fadeOut(200)
+
+    //     function hideContent() {
+    //         setTimeout(function() {
+    //             $('#content').removeClass('show');
+    //         }, 200);
+    //     };
+    //     hideContent();
+    // });
+
+
+    // var video = $('#sbb-video');
+    //    $('#volume-button').click(function videoSound() {
+    //        if (video.hasAttribute("muted")) {
+    //            video.removeAttribute("muted")
+    //        } else {
+    //            video.setAttribute("muted", "muted")
+    //        };
+    //    });
+
+    // FADE IN CONTENT
+    // $('#content').hide();
+    // $('#menu-button').click(function() {
+    //     $('#content').delay(100).fadeIn(300)
+    //     $('#menu-button').fadeOut(500)
+    // });
+
+
+    // FADE OUT CONTENT
+    // $('#video-button').click(function() {
+    //     $('#menu-button').fadeIn(300)
+    //     $('#content').delay(100).fadeOut(300)
+
 });
